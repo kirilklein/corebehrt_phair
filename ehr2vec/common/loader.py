c63efbb1a5912cd959b97dabe14a64e4aea9e882
Kiril Klein <kikl@di.ku.dk>
2024-10-11 18:04:50 +0200

    Fix loader

--- ehr2vec/common/loader.py
+++ ehr2vec/common/loader.py
@@ -284,12 +284,11 @@ def load_counterfactual_outcomes(counterfactual_outcome_path: str) -> pd.DataFra
     return counterfactual_outcomes
 
 
-def load_predictions_from_finetune_dir(finetune_dir: str) -> pd.DataFrame:
+def load_predictions_from_finetune_dir(finetune_dir: str, calibrated: bool = False) -> pd.DataFrame:
     """Load predictions from finetune directory."""
-    check_path(finetune_dir, "predictions_and_targets.npz")
-    pred_and_targets = np.load(join(finetune_dir, "predictions_and_targets.npz"))
-    return pd.DataFrame({k: v.flatten() for k, v in pred_and_targets.items()})
-
+    file = "predictions_and_targets_calibrated.csv" if calibrated else "predictions_and_targets.csv"
+    return pd.read_csv(join(finetune_dir, file))
+    
 
 def load_index_dates(finetune_dir: str) -> pd.DataFrame:
     """Load index dates from finetune directory."""
