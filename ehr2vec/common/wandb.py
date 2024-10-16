f85b0154c82b7aca5e9a4ae5eb3ea0e855bb37b6
Kiril Klein <kikl@di.ku.dk>
2024-10-16 10:27:00 +0200

    Add wandb logging of estimate_effect
    + log paths

--- ehr2vec/common/wandb.py
+++ ehr2vec/common/wandb.py
@@ -4,36 +4,49 @@ try:
     use_wandb = True
 except ImportError:
     use_wandb = False
+from ehr2vec.common.config import Config
 
 
-def initialize_wandb(run, cfg, wandb_kwargs):
+def initialize_wandb(run, cfg: Config, wandb_kwargs: dict) -> wandb.run:
     """
     Initialize Wandb if available, else return run.
     Return the run object.
     """
     if not use_wandb:
         return run
-    else:
-        wandb_config = {
-            key: {}
-            for key in [
-                "trainer_args",
-                "model",
-                "optimizer",
-                "data",
-                "test_args",
-                "scheduler",
-                "outcome",
-            ]
-        }
-        for key in wandb_config.keys():
-            if hasattr(cfg, key):
-                wandb_config[key].update(cfg[key])
-        wandb.init(config=wandb_config, **wandb_kwargs)
-        run = wandb.run
-        return run
+
+    wandb_config = create_wandb_config(cfg)
+    wandb.init(config=wandb_config, **wandb_kwargs)
+    return wandb.run
 
 
-def finish_wandb():
+def create_wandb_config(cfg: Config) -> dict:
+    """
+    Create a wandb config dictionary from a Config object.
+    """
+    config_keys = [
+        "trainer_args",
+        "model",
+        "optimizer",
+        "data",
+        "test_args",
+        "scheduler",
+        "outcome",
+        "estimator",
+        "paths",
+        "ps_file",
+    ]
+    wandb_config = {key: {} for key in config_keys}
+
+    for key in wandb_config:
+        if hasattr(cfg, key):
+            wandb_config[key] = (
+                cfg[key] if isinstance(cfg[key], dict) else {"value": cfg[key]}
+            )
+
+    return wandb_config
+
+
+def finish_wandb() -> None:
     if use_wandb:
         wandb.finish()
