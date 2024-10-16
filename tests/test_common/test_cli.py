25c1cb2801d0f58a4a5c24b456b660836d1e6c94
Kiril Klein <kikl@di.ku.dk>
2024-10-16 11:51:31 +0200

    add unittest for cli

--- tests/test_common/test_cli.py
+++ tests/test_common/test_cli.py
@@ -1,94 +1,9 @@
+import sys
 import unittest
 from unittest.mock import patch
-import sys
 
-# Assuming the code above is in the same module or imported appropriately
-# Since we need a Config class for testing, we'll define a minimal version here
-
-
-class Config(dict):
-    """Config class that allows for dot notation."""
-
-    def __init__(self, dictionary=None):
-        super().__init__()
-        if dictionary:
-            for key, value in dictionary.items():
-                if isinstance(value, dict):
-                    value = Config(value)
-                self[key] = value
-                setattr(self, key, value)
-
-    def __setattr__(self, key, value):
-        super().__setattr__(key, value)
-        super().__setitem__(key, value)
-
-    def __setitem__(self, key, value):
-        super().__setitem__(key, value)
-        super().__setattr__(key, value)
-
-    def str_to_num(self, s):
-        """Converts a string to an int or float if possible."""
-        try:
-            return int(s)
-        except ValueError:
-            try:
-                return float(s)
-            except ValueError:
-                return s
-
-
-# The functions to be tested
-def override_config_from_cli(cfg):
-    """
-    Overrides the configuration object with values provided from the CLI.
-    """
-    overrides = parse_cli_args()
-    for key, value, is_new in overrides:
-        value = cfg.str_to_num(value)
-        set_nested_attr(cfg, key, value, allow_new=is_new)
-
-
-def parse_cli_args():
-    """
-    Parses command-line arguments in the form 'key=value' or '+key=value'.
-    Returns a list of tuples: (key, value, is_new)
-    """
-    args = sys.argv[1:]
-    overrides = []
-    for arg in args:
-        if "=" in arg:
-            key, value = arg.split("=", 1)
-            is_new = False
-            if key.startswith("+"):
-                is_new = True
-                key = key[1:]  # Remove '+' prefix
-            overrides.append((key, value, is_new))
-        else:
-            raise ValueError(f"Argument '{arg}' is not in 'key=value' format.")
-    return overrides
-
-
-def set_nested_attr(obj, attr_path, value, allow_new=False):
-    """
-    Sets a nested attribute in a Config object based on a dot-separated path.
-    If allow_new is True, it will create new attributes as needed.
-    """
-    attrs = attr_path.split(".")
-    for attr in attrs[:-1]:
-        if not hasattr(obj, attr):
-            if allow_new:
-                setattr(obj, attr, Config())
-            else:
-                raise AttributeError(
-                    f"Attribute '{attr}' does not exist in the configuration."
-                )
-        obj = getattr(obj, attr)
-    if hasattr(obj, attrs[-1]) or allow_new:
-        setattr(obj, attrs[-1], value)
-    else:
-        raise AttributeError(
-            f"Attribute '{attrs[-1]}' does not exist in the configuration."
-        )
+from ehr2vec.common.cli import override_config_from_cli, parse_cli_args, set_nested_attr
+from ehr2vec.common.config import Config
 
 
 # Unit tests
