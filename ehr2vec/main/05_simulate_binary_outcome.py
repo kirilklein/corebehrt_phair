6108cc83e7732f5794bacafe4ad05cab2aee2f33
Kiril Klein <kikl@di.ku.dk>
2024-11-26 14:03:28 +0100

    Fix setup, so outpu isn't overwritten by finetune_folder

--- ehr2vec/main/05_simulate_binary_outcome.py
+++ ehr2vec/main/05_simulate_binary_outcome.py
@@ -13,7 +13,7 @@ from ehr2vec.common.default_args import DEFAULT_BLOBSTORE
 from ehr2vec.common.loader import load_config, load_index_dates
 from ehr2vec.common.setup import (
     get_args,
-    initialize_configuration_finetune,
+    initialize_configuration_effect_estimation,
     setup_logger,
 )
 from ehr2vec.simulation.longitudinal_outcome import simulate_abspos_from_binary_outcome
@@ -33,12 +33,13 @@ config_path = join(dirname(dirname(abspath(__file__))), args.config_path)
 def main(config_path: str) -> None:
     cfg: Config = load_config(config_path)
     override_config_from_cli(cfg)
-    cfg, run, mount_context, pretrain_model_path = initialize_configuration_finetune(
+    cfg, _, mount_context, _ = initialize_configuration_effect_estimation(
         cfg, dataset_name=cfg.get("project", DEFAULT_BLOBSTORE)
     )
     simulation_folder = cfg.paths.output_path
     if cfg.env == "azure":
         simulation_folder = join(simulation_folder, cfg.paths.run_name)
+
     os.makedirs(simulation_folder, exist_ok=True)
     logger = setup_logger(simulation_folder)
 
