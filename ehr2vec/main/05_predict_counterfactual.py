07cf2b41471951c15e9f4430eb3bd7d427456d18
Kiril Klein <kikl@di.ku.dk>
2024-12-13 16:00:16 +0100

    Provide azurecontext

--- ehr2vec/main/05_predict_counterfactual.py
+++ ehr2vec/main/05_predict_counterfactual.py
@@ -161,7 +161,7 @@ def main(config_path: str):
             local_path="",  # uses everything in 'outputs'
             remote_path=join(
                 cfg.get("project", DEFAULT_BLOBSTORE),
-                fix_tmp_prefixes_for_azure_paths(cfg.paths.model_path),
+                fix_tmp_prefixes_for_azure_paths(cfg.paths.model_path, azure_context),
             ),
         )
         mount_context.stop()
