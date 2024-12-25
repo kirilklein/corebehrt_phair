7788270fae0a3a347788fa22d0c0aca02270f985
Kiril Klein <kikl@di.ku.dk>
2024-12-25 16:00:51 +0100

    improve l1 regularization

--- ehr2vec/trainer/trainer.py
+++ ehr2vec/trainer/trainer.py
@@ -211,11 +211,9 @@ class EHRTrainer:
                 unscaled_loss = outputs.loss
 
                 # Add L1 regularization if lambda > 0
-                if self.l1_lambda > 0:
-                    l1_loss = 0
-                    for param in self.model.parameters():
-                        l1_loss += torch.sum(torch.abs(param))
-                    unscaled_loss += self.l1_lambda * l1_loss
+                l1_loss = self._compute_l1_loss()
+                self.run_log("L1 loss", l1_loss.item())
+                unscaled_loss += l1_loss
 
                 scaled_loss = self.scaler.scale(unscaled_loss)
             scaled_loss.backward()
@@ -225,15 +223,20 @@ class EHRTrainer:
             unscaled_loss = outputs.loss
 
             # Add L1 regularization if lambda > 0
-            if self.l1_lambda > 0:
-                l1_loss = 0
-                for param in self.model.parameters():
-                    l1_loss += torch.sum(torch.abs(param))
-                unscaled_loss += self.l1_lambda * l1_loss
+            l1_loss = self._compute_l1_loss()
+            self.run_log("L1 loss", l1_loss.item())
+            unscaled_loss += l1_loss
 
             unscaled_loss.backward()
 
         return unscaled_loss
+
+    def _compute_l1_loss(self):
+        if self.l1_lambda <= 0:
+            return 0
+        return self.l1_lambda * sum(
+            torch.sum(torch.abs(p)) for p in self.model.parameters()
+        )
 
     def _update_and_log(self, step_loss, train_loop, epoch_loss):
         """Updates the model and logs the loss"""
