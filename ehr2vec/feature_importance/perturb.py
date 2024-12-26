f5949f9e0a16ea1fcd4cf886293967cc35b38f5c
kirilklein <kleinkiril94@gmail.com>
2024-12-26 15:01:46 +0100

    make sure to use dict access

--- ehr2vec/feature_importance/perturb.py
+++ ehr2vec/feature_importance/perturb.py
@@ -51,11 +51,11 @@ class PerturbationModel(torch.nn.Module):
         )
         loss = self.perturbation_loss(original_output, perturbed_output, batch)
         outputs = ModelOutputs(
-            logits=original_output.logits,
-            perturbed_logits=perturbed_output.logits,
+            logits=original_output["logits"],
+            perturbed_logits=perturbed_output["logits"],
             loss=loss,
-            hidden_states=original_output.hidden_states,
-            perturbed_hidden_states=perturbed_output.hidden_states,
+            hidden_states=original_output["hidden_states"],
+            perturbed_hidden_states=perturbed_output["hidden_states"],
         )
         return outputs
 
